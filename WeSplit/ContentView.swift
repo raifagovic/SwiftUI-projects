8bae1c584a5a55bd6065cd8e29034a41c88f0849
Raif Agovic <raif.agovic.dev@gmail.com>
2023-11-09 21:32:23 +0100

    NavigationStack

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,18 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form {
-            Section {
-                Text("Hello World")
-                Text("Hello World")
-            }
-            Section {
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
-                Text("Hello World")
+        NavigationStack {
+            Form {
+                Section {
+                    Text("Hello World")
+                    Text("Hello World")
+                }
+                Section {
+                    Text("Hello World")
+                    Text("Hello World")
+                    Text("Hello World")
+                    Text("Hello World")
+                }
             }
         }
+        .navigationTitle("Your Title")
     }
 }
 
