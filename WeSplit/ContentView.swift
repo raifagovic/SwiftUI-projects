b353af57f05f2a843f678d0e14ad12497e65ea43
Raif Agovic <raif.agovic.dev@gmail.com>
2023-11-11 15:51:47 +0100

    binding state

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,12 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var tapCount = 0
+    @State private var name = ""
 
     var body: some View {
-        Button("Tap Count: \(tapCount)") {
-        tapCount += 1
+        Form {
+            TextField("Enter your name: ", text: $name)
+            Text("Hi!")
         }
     }
 }
+
 
