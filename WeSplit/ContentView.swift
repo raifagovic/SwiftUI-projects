6216a0a208b54f514a897e3884eb736baad9fdbf
Raif Agovic <raif.agovic.dev@gmail.com>
2023-11-11 10:22:10 +0100

    corrected Button

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,8 +11,9 @@ struct ContentView: View {
     @State private var tapCount = 0
 
     var body: some View {
-        Button("Tap Count: \(tapcount)")
+        Button("Tap Count: \(tapCount)") {
         tapCount += 1
+        }
     }
 }
 
