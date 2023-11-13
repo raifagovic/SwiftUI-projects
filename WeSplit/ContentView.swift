b28f79e2f83b6c1ce7347bef83ddd8094c183343
Raif Agovic <raif.agovic.dev@gmail.com>
2023-11-13 22:53:12 +0100

    insert checkAmount

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,20 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    let students = ["Paul", "John", "Tom"]
-    @State private var selectedStudent = "Paul"
-
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        NavigationStack {
-            Form {
-                Picker("Select your student", selection: $selectedStudent) {
-                    ForEach(students, id: \.self) {
-                        Text($0)
-                    }
-                }
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                    .keyboardType(.decimalPad)
             }
         }
     }
+    
 }
 
 
