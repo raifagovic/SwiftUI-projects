5238dbb71a5eb9cb788c9b3bd1b4ec6e034b61ee
Raif Agovic <raif.agovic.dev@gmail.com>
2023-11-15 21:39:36 +0100

    Create pickers in a form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -5,24 +5,51 @@
 //  Created by Raif on 8. 11. 2023..
 //
 
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Paul Hudson on 07/10/2023.
+//
+
 import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    
+
     let tipPercentages = [10, 15, 20, 25, 0]
-    
+
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    .keyboardType(.decimalPad)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+
+                Section("How much do you want to tip?") {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
             }
+            .navigationTitle("WeSplit")
         }
     }
-    
 }
-
-
