f417b3d5f2f1b0c051ba2a1fa38c8ec3563ce740
Raif Agovic <raif.agovic.dev@gmail.com>
2023-11-16 23:37:42 +0100

    Hide the keyboard

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -5,21 +5,26 @@
 //  Created by Raif on 8. 11. 2023..
 //
 
-//
-//  ContentView.swift
-//  WeSplit
-//
-//  Created by Paul Hudson on 07/10/2023.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
 
     let tipPercentages = [10, 15, 20, 25, 0]
+
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+
+        return amountPerPerson
+    }
 
     var body: some View {
         NavigationStack {
@@ -50,6 +55,13 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
     }
 }
