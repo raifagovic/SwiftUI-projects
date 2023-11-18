98307eab253d46724acf9c082fcc59819caa8c0a
Raif Agovic <raif.agovic.dev@gmail.com>
2023-11-18 11:08:32 +0100

    Use Spacer()

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Spacer()
+            Text("First")
+            Text("Second")
+            Text("Third")
+            Spacer()
+            Spacer()
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
+
