9ae1283f3acff747562b86ec126a7fb160e08848
Raif Agovic <raif.agovic.dev@gmail.com>
2023-11-23 13:32:14 +0100

    Initial commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Raif on 23. 11. 2023..
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Raif on 23. 11. 2023..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
