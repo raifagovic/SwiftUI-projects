b9452f8edd245d26eb9ff011f5e74f4f382af848
Raif Agovic <raif.agovic.dev@gmail.com>
2023-12-02 21:44:27 +0100

    Add transition

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,17 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var animationAmount = 0.0
-    
+    @State private var isShowingRed = false
+
     var body: some View {
-        Button("Tap Me") {
-            enabled.toggle()
+        VStack {
+            Button("Tap Me") {
+                withAnimation {
+                    isShowingRed.toggle()
+                }
+            }
+
+            if isShowingRed {
+                Rectangle()
+                    .fill(.red)
+                    .frame(width: 200, height: 200)
+                    .transition(.asymmetric(insertion: .scale, removal: .opacity))
+            }
         }
-        .frame(width: 200, height: 200)
-        .background(enabled ? .blue : .red)
-        .animation(.default, value: enabled)
-        .foregroundStyle(.white)
-        .clipShape(.rect(cornerRadius: enabled ? 60 : 0))
-        .animation(.spring(duration: 1, bounce: 0.6), value: enabled)
     }
 }
