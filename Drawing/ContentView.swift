14e7e36b7a2ad7252e139b1bfc241a798146e10d
Raif Agovic <raif.agovic.dev@gmail.com>
2023-12-23 10:53:43 +0100

    Initial commit

---
+++ Drawing/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Raif on 23. 12. 2023..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
---
+++ Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Raif on 23. 12. 2023..
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
