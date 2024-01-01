e6ba75ffbf0e959fbdcaef4fd24c0008b0a1af8b
Raif Agovic <raif.agovic.dev@gmail.com>
2024-01-01 23:07:19 +0100

    Add ProgressView() to AsyncImage

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -10,12 +10,17 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-            image
-                .resizable()
-                .scaledToFit()
-        } placeholder: {
-            Color.red
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
+            }
         }
-        .frame(width: 200, height: 200)    }
+        .frame(width: 200, height: 200)
+    }
 }
