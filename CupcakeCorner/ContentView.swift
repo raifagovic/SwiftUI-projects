c703c1a7b47ce48c43aa47cad36477d3793ea73e
Raif Agovic <raif.agovic.dev@gmail.com>
2024-01-02 10:16:55 +0100

    Make a form

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,19 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-    
+    @State private var username = ""
+    @State private var email = ""
+
     var body: some View {
-        AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { phase in
-            if let image = phase.image {
-                image
-                    .resizable()
-                    .scaledToFit()
-            } else if phase.error != nil {
-                Text("There was an error loading the image.")
-            } else {
-                ProgressView()
+        Form {
+            Section {
+                TextField("Username", text: $username)
+                TextField("Email", text: $email)
+            }
+
+            Section {
+                Button("Create account") {
+                    print("Creating account…")
+                }
             }
         }
-        .frame(width: 200, height: 200)
     }
 }
